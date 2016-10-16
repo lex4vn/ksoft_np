79aece071baaa9fdfb7195c8bec4a79103cebf1e
Nguyen Duc Tho <lex4vn@gmail.com>
2016-10-17 01:18:15 +0700

    footer header

---
+++ wp-content/themes/ngon/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ */
+		do_action( 'cherry_footer_before' );
+
+		do_action( 'cherry_footer' );
+
+		do_action( 'cherry_footer_after' ); ?>
+
+	</div><!--site-wrapper-->
+
+<?php do_action( 'cherry_body_end' ); ?>
+<div id="page-mask" class="page-mask"></div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/ngon/header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying theme header.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<?php wp_head(); ?>
+</head>
+<body <?php cherry_attr( 'body' ); ?>>
+
+	<?php do_action( 'cherry_body_start' ); ?>
+	<?php do_action( 'cherry_header_before' ); ?>
+
+	<?php do_action( 'cherry_header' ); ?>
+
+	<?php do_action( 'cherry_header_after' ); ?>
+	<div id="wrapper" class="hfeed site">
+
