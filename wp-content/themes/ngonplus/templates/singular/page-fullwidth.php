d5779cbf689d6aaaee416b996cbf3e31db20d577
Nguyen Duc Tho <lex4vn@gmail.com>
2016-10-20 22:12:45 +0700

    remove ngonplus

--- wp-content/themes/ngonplus/templates/singular/page-fullwidth.php
+++
@@ -1,9 +0,0 @@
-<?php news_mix_lite_breadcrumb(); ?>
-
-<div class="row-fluid">
-    <div class="span12">
-        <?php get_template_part( 'templates/loop/loop', 'page' ); ?>
-    </div>
-</div>    
-
-<div class="clear"></div>
